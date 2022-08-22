01890298d5b26d09bf507a17c27e498a137a4474
Pasquale Ranalli <pranalli@gmail.com>
2022-08-22 13:53:02 -0400

    Update StringPermutationIdentifier.java
    Code cleanup

--- src/com/pranalli/portfolio/exercises/StringPermutationIdentifier.java
+++ src/com/pranalli/portfolio/exercises/StringPermutationIdentifier.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 /**
  * <h1>Exercise - String Permutation Identifier</h1>
  * <p>
- *     This problem is defined as identifying two strings that are permutations of one-another.
+ * This problem is defined as identifying two strings that are permutations of one-another.
  * </p>
  *
  * @author Pasquale A. Ranalli
@@ -15,47 +15,47 @@ import java.util.Arrays;
  */
 class StringPermutationIdentifier {
 
-	/**
-	 * This method returns a boolean value indicating if the two strings are permutations of one-another.
-	 * Flags for case and space (white space) sensitivity can be used as desired.
-	 *
-	 * @param s1 String
-	 * @param s2 String
-	 * @param caseInsensitive Set to true to enable case-insensitivity i.e., 'I' == 'i'.
-	 * @param spaceInsensitive Set to true to enable whitespace-insensitivity where leading or trailing whitespace will
-	 *                         be excluded from the comparison.
-	 * @return Whether the given strings are permutations of one-another, taking into account the above flags.
-	 */
-	public static boolean isPermutation(String s1, String s2, boolean caseInsensitive, boolean spaceInsensitive) {
-	    if (caseInsensitive) {
-	    	return isPermutation(s1.toLowerCase(), s2.toLowerCase(), false, spaceInsensitive);
-		}
+    /**
+     * This method returns a boolean value indicating if the two strings are permutations of one-another.
+     * Flags for case and space (white space) sensitivity can be used as desired.
+     *
+     * @param s1               String
+     * @param s2               String
+     * @param caseInsensitive  Set to true to enable case-insensitivity i.e., 'I' == 'i'.
+     * @param spaceInsensitive Set to true to enable whitespace-insensitivity where leading or trailing whitespace will
+     *                         be excluded from the comparison.
+     * @return Whether the given strings are permutations of one-another, taking into account the above flags.
+     */
+    public static boolean isPermutation(String s1, String s2, boolean caseInsensitive, boolean spaceInsensitive) {
+        if (caseInsensitive) {
+            return isPermutation(s1.toLowerCase(), s2.toLowerCase(), false, spaceInsensitive);
+        }
 
-	    if (spaceInsensitive) {
-	    	return isPermutation(s1.trim(), s2.trim(), false, false);
-		}
+        if (spaceInsensitive) {
+            return isPermutation(s1.trim(), s2.trim(), false, false);
+        }
 
-		if (s1.length() != s2.length()) {
-			return false;
-		}
+        if (s1.length() != s2.length()) {
+            return false;
+        }
 
-		return Arrays.equals(
-			SortingUtils.insertionSort(s1.toCharArray()),
-			SortingUtils.insertionSort(s2.toCharArray())
-		);
-	}
+        return Arrays.equals(
+                SortingUtils.insertionSort(s1.toCharArray()),
+                SortingUtils.insertionSort(s2.toCharArray())
+        );
+    }
 
-	// Test cases
-	public static void main(String[] args) {
-		String s1 = "I am Groot";
-		String s2 = "Am I groot?";
-		String s3 = "I am groot";
-		String s4 = "I am Groot   ";
-		boolean caseInsensitive = true;
-		boolean spaceInsensitive = false;
+    // Test cases
+    public static void main(String[] args) {
+        String s1 = "I am Groot";
+        String s2 = "Am I groot?";
+        String s3 = "I am groot";
+        String s4 = "I am Groot   ";
+        boolean caseInsensitive = true;
+        boolean spaceInsensitive = false;
 
-		System.out.println(isPermutation(s1, s2, caseInsensitive, spaceInsensitive));
-		System.out.println(isPermutation(s1, s3, caseInsensitive, spaceInsensitive));
-		System.out.println(isPermutation(s1, s4, caseInsensitive, spaceInsensitive));
-	}
+        System.out.println(isPermutation(s1, s2, caseInsensitive, spaceInsensitive));
+        System.out.println(isPermutation(s1, s3, caseInsensitive, spaceInsensitive));
+        System.out.println(isPermutation(s1, s4, caseInsensitive, spaceInsensitive));
+    }
 }
